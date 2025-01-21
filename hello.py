fb196363832a75d2fbf5b553d905af3107178a3e
OTeeEnabor <tobienabor@gmail.com>
2025-01-21 15:10:38 +0200

    made a change

--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
 def greet_user()-> None:
     print("hello guys")
 
-greet_user()
+def check_age_function():
+    age = input('Enter Your Age:')
+
+    if int(age) >= 18 and int(age) < 21:
+        print('Welcome to a diploma degree')
+    elif int(age) >= 21:
+        print('Welcome to a uni degree')   
+    else:
+        print('Not allowed')
+    
+# greet_user()
+
+check_age_function()
